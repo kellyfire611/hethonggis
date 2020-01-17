f6ebb3c8f66164fd986df9ffc4eda12b4ea2a0c1
kellyfire611 <kellyfire611@gmail.com>
2020-01-17 22:31:41 +0700

    add source

--- src/app/Http/Controllers/Backend/DashboardController.php
+++ src/app/Http/Controllers/Backend/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\DiaDiem;
+use App\Models\TinhThanh;
+use App\Models\QuangCao;
+use App\Models\Auth\User;
 
 /**
  * Class DashboardController.
@@ -14,6 +18,50 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('backend.dashboard');
+        $baocao= [];
+        $diadiems = DiaDiem::all();
+        $baocao['diadiem_count'] = $diadiems->count();
+
+        $baocao['dichvu_count'] = 0;
+        foreach($diadiems as $key=>$value)
+        {
+            $baocao['dichvu_count'] += $value->dichvus->count();
+        }
+
+        $users = User::all();
+        $baocao['user_count'] = $users->count();
+
+        $tinhthanhs = TinhThanh::all();
+        $baocao['tinhthanh_count'] = $tinhthanhs->count();
+
+        $tongsoluotdanhgia = 0;
+        foreach($diadiems as $diadiem)
+        {
+            $diem = 0;
+            $sumDiem = 0;
+            foreach($diadiem->danhgias()->get() as $key=>$value)
+            {
+                $sumDiem += empty($value->diem) ? 0 : $value->diem;
+            }
+            if($diadiem->danhgias()->count() <= 0)
+            {
+                $diadiem->diemtrungbinh = 0;
+            }
+            else
+            {
+                $diadiem->diemtrungbinh = $sumDiem / $diadiem->danhgias()->count();
+            }
+            $tongsoluotdanhgia += $diadiem->danhgias()->count();
+        }
+        
+        $quangcaos = QuangCao::all();
+
+        $top5diadiems = $diadiems->sortByDesc('diemtrungbinh')->take(5);
+
+        return view('backend.dashboard')
+            ->with('baocaodata', $baocao)
+            ->with('top5diadiems', $top5diadiems)
+            ->with('quangcaos', $quangcaos)
+            ->with('tongsoluotdanhgia', $tongsoluotdanhgia);
     }
 }
--- src/app/Http/Controllers/Frontend/HomeController.php
+++ src/app/Http/Controllers/Frontend/HomeController.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use Illuminate\Http\Request;
+use App\Http\Requests\Backend\DiaDiem\ManageDiaDiemRequest;
+use App\Models\DiaDiem;
+use App\Models\DichVu;
+use App\Models\DiaChi;
+use App\Models\TinhThanh;
+use App\Models\QuanHuyen;
+use App\Models\XaPhuong;
+use App\Models\QuangCao;
 use App\Http\Controllers\Controller;
+use App\Repositories\Backend\DiaDiemRepository;
 
 /**
  * Class HomeController.
@@ -10,10 +20,82 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     /**
+     * @var DiaDiemRepository
+     */
+    protected $DiaDiemRepository;
+
+    /**
+     * DiaDiemController constructor.
+     *
+     * @param DiaDiemRepository $DiaDiemRepository
+     */
+    public function __construct(DiaDiemRepository $DiaDiemRepository)
+    {
+        $this->DiaDiemRepository = $DiaDiemRepository;
+    }
+
+    /**
      * @return \Illuminate\View\View
      */
     public function index()
     {
-        return view('frontend.index');
+        $diadiems = DiaDiem::take(12)->get();
+
+        foreach($diadiems as $diadiem)
+        {
+            $diem = 0;
+            $sumDiem = 0;
+            foreach($diadiem->danhgias()->get() as $key=>$value)
+            {
+                $sumDiem += empty($value->diem) ? 0 : $value->diem;
+            }
+            if($diadiem->danhgias()->count() <= 0)
+            {
+                $diadiem->diemtrungbinh = 0;
+            }
+            else
+            {
+                $diadiem->diemtrungbinh = $sumDiem / $diadiem->danhgias()->count();
+            }
+        }
+        $topmonans = DiaDiem::all();
+        foreach($topmonans as $diadiem)
+        {
+            $diem = 0;
+            $sumDiem = 0;
+            foreach($diadiem->danhgias()->get() as $key=>$value)
+            {
+                $sumDiem += empty($value->diem) ? 0 : $value->diem;
+            }
+            if($diadiem->danhgias()->count() <= 0)
+            {
+                $diadiem->diemtrungbinh = 0;
+            }
+            else
+            {
+                $diadiem->diemtrungbinh = $sumDiem / $diadiem->danhgias()->count();
+            }
+        }
+        $quangcaos = QuangCao::all();
+
+        $diadiems = $diadiems->sortByDesc('diemtrungbinh');
+        return view('frontend.index')
+            ->with('diadiems', $diadiems)
+            ->with('topmonans', $topmonans)
+            ->with('quangcaos', $quangcaos);
+    }
+
+    public function search(Request $request)
+    {
+        $inputs = $request->only(
+            'type_search',
+            'keyword'
+        );
+
+        $inputs['keyword'] = trim($inputs['keyword']);
+        $diadiems = $this->DiaDiemRepository->search($inputs);
+        return view('frontend.search')
+            ->with('diadiems', $diadiems)
+            ->with('inputs', $inputs);
     }
 }
